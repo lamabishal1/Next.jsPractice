7df7506685dc654bd004723b524fcfd5bb995c84
LAMA BISHAL <lb1627565@gmail.com>
2025-07-03 19:38:56 +0900

    templates

--- app/(auth)/template.jsx
+++ app/(auth)/template.jsx
@@ -5,6 +5,7 @@ import Footer from '../components/Footer'
 import Link from 'next/link'
 import './styles.css'
 import { usePathname } from 'next/navigation'
+import { useState } from 'react'
 
 
 const navLinks = [
@@ -23,15 +24,20 @@ const navLinks = [
 ]
 
 const AuthLayout = ({children}) => {
+  const [input, setInput] = useState('');
   const pathname = usePathname();
   return (
     <>
-        <div>
+    <div className='p-3 my-2 border border-black w-full max-w-md mx-auto '>
+      <input value={input} onChange={(e) => setInput(e.target.value)}
+      />
+    </div>
+        <div className='flex justify-center gap-5'>
           {navLinks.map((link)=>{
             const isActive = pathname === link.href || (pathname.startsWith(link.href) && pathname !== '/');
             return(
               <Link href={link.href} key={link.title}
-              className={`p-3 ${isActive ? "text-red-500 font-bold underline" : "bg-white text-black"}`}>
+              className={`${isActive ? "text-red-500 font-bold underline" : "bg-white text-black"}`}>
                 {link.title}
               </Link>
             )
@@ -40,7 +46,6 @@ const AuthLayout = ({children}) => {
         <div className='mt-10 p-5 text-3xl font-bold'>
         {children}
         </div>
-          <Footer />
     </>
   )
 }
